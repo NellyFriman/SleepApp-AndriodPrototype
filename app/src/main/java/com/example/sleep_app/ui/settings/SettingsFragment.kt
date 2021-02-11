dc0d8a38a54c9120fdaecaf98f666cea7b827449
hector gonzalez campos <hector-99hgc@hotmail.com>
2021-02-11 13:31:15 +0100

    Settings update

--- app/src/main/java/com/example/sleep_app/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/sleep_app/ui/settings/SettingsFragment.kt
@@ -23,7 +23,7 @@ class SettingsFragment : Fragment() {
         settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
+        val textView: TextView = root.findViewById(R.id.textSettings)
         settingsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
