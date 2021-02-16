5a3aa63b957d588fa8206e44014946e11e99370b
RonasB <36659008+RonasB@users.noreply.github.com>
2021-02-16 11:53:42 +0100

    added activities for the three task phases

---
+++ app/src/main/java/com/example/sleep_app/Encoding.kt
@@ -0,0 +1,11 @@
+package com.example.sleep_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Encoding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encoding)
+    }
+}
